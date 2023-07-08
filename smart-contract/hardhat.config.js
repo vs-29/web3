6057333d76a5fe5dcfc670089246231f7e7d8cf0
vaishnavi <pawarvaishnavi196@gmail.com>
2023-07-08 16:10:35 +0530

    network change

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,15 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const GOERLI_URL=process.env.GOERLI_URL;
+const POLYGON_URL=process.env.POLYGON_URL;
 const PRIVATE_KEY=process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks:{
-    goerli:{
-      url:GOERLI_URL,
+    polygon:{
+      url:POLYGON_URL,
       accounts:[PRIVATE_KEY]
     }
   }
