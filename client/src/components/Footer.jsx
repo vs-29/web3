a3d4df90302965b6e6436e141357839ad4fb5d75
vaishnavi <pawarvaishnavi196@gmail.com>
2023-07-08 14:07:06 +0530

    changes made

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 import logo from "../../images/logo.png";
 
@@ -8,23 +8,17 @@ const Footer = () => (
       <div className="flex flex-[0.5] justify-center items-center">
         <img src={logo} alt="logo" className="w-32" />
       </div>
-      <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
-      </div>
+    
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5">
-      <p className="text-white text-sm text-center">Come join us and hear for the unexpected miracle</p>
-      <p className="text-white text-sm text-center font-medium mt-2">info@kryptoexpertise.com</p>
+      <p className="text-white text-sm text-center">Developing New stairs for Web3</p>
     </div>
 
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-      <p className="text-white text-left text-xs">@kryptoexpertise2022</p>
+      <p className="text-white text-left text-xs">@kryptoexpertise2023</p>
       <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
   </div>
